14623865bab386c78389bc7aba8d733f3f9f9caf
Amanda Vieira <amanda.alexandre@outlook.com>
2022-09-24 03:13:07 -0300

    feat: requested country list from API

---
+++ src/services/Request.jsx
@@ -0,0 +1,25 @@
+import {React, useState, useEffect} from 'react'
+import axios from 'axios'
+
+function Request() {
+    // Axios parameters
+    const urlCountries = 'https://api.covid19api.com/countries'
+
+    const [countries, setCountries] = useState([]);
+
+    // Makes request with Axios and useEffect
+    useEffect(() => {
+        axios.get(urlCountries).then(res => {
+            console.log(res.data)
+            setCountries(res.data);
+        })
+    }, [])
+
+  return (
+    <div>
+        {countries.map(country => <p key={country.IS02}>{country.Country}</p>)}
+    </div>
+  )
+}
+
+export default Request
