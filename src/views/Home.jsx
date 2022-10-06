ff847f115a86654390db489598a222e9ff2f4744
Amanda Vieira <amanda.alexandre@outlook.com>
2022-10-06 06:12:59 -0300

    style: hero section

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import '../assets/home.css'
+import CovidImg from '../assets/covid_hero_image.png'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='home-hero'>
+
+        <div className='home-hero-title'>
+          <h1>Covid-19 Info</h1>
+          <h2>Check all cases by country</h2>
+          <button>Check data</button>
+        </div>
+
+        <div className='home-hero-rotate-container'>
+          <div className='home-hero-square-white'></div>
+          <div className='home-hero-square-blue'>
+            <img src={CovidImg} className='home-hero-covid-img' />
+          </div>
+      
+        </div>
+      
+      </div>
+    </div>
   )
 }
 
