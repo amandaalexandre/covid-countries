8f322ca25ea47fd4f8f4fb8aba851ba11955471f
Amanda Vieira <amanda.alexandre@outlook.com>
2022-10-06 07:13:42 -0300

    style: navlinks & responsivity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import Logo from '../assets/logo.png'
 import '../assets/navbar.css'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 function Navbar() {
+
+  let activeLink = 'navbar-link-active';
+  let unactiveLink = 'navbar-link';
 
   return (
     <header>
         <span className='navbar-links'>
-          <Link to="/">Home</Link>
-          <Link to="/dashboard">Dashboard</Link>
-          <Link to="/credits">Credits</Link>
+          <NavLink to="/" end className={({ isActive }) =>
+              isActive ? activeLink : unactiveLink}>Home</NavLink>
+          <NavLink to="/dashboard" className={({ isActive }) =>
+              isActive ? activeLink : unactiveLink}>Dashboard</NavLink>
+          <NavLink to="/credits" className={({ isActive }) =>
+              isActive ? activeLink : unactiveLink}>Credits</NavLink>
         </span>
         <img src={Logo} className='logo' />
     </header>
