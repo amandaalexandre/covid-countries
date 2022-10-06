499ed5dfcc766c1f42ceb4f0fb2a83a460e26516
Amanda Vieira <amanda.alexandre@outlook.com>
2022-10-06 04:30:13 -0300

    feat: Credits page

--- src/views/Credits.jsx
+++ src/views/Credits.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Credits() {
   return (
-    <div>Credits</div>
+    <div>
+      <h1>Credits</h1>
+      <b>Developer:</b> <a href="https://www.linkedin.com/in/amandavieiradev/">Amanda Vieira</a>
+      <br/>
+      <b>API for Covid data: </b><a href="https://documenter.getpostman.com/view/10808728/SzS8rjbc">Covid 19 API</a>
+      <br/>
+      <b>Stack: </b>React, React Router, Context API, vanilla CSS
+    </div>
   )
 }
 
