204bd48613d79bbc9c133c3df1e549af9c3bbac9
Amanda Vieira <amanda.alexandre@outlook.com>
2022-10-10 08:34:35 -0300

    style: making links in credits page more visible

--- src/views/Credits.jsx
+++ src/views/Credits.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../assets/credits.css'
 
 function Credits() {
   return (
-    <div>
+    <div className='credits'>
       <h1>Credits</h1>
       <b>Developer:</b> <a href="https://www.linkedin.com/in/amandavieiradev/">Amanda Vieira</a>
       <br/>
       <b>API for Covid data: </b><a href="https://rapidapi.com/vaccovidlive-vaccovidlive-default/api/vaccovid-coronavirus-vaccine-and-treatment-tracker/">VACCOVID - coronavirus, vaccine and treatment tracker</a>
       <br/>
-      <b>Stack: </b>React, React Router, Context API, vanilla CSS
+      <b>Stack: </b>React, React Router, vanilla CSS
     </div>
   )
 }
